adad055ea276f1c014b70781b530e43b0b7c6e1e
Jaqueline <jaqueline.bonoto@dbccompany.com.br>
2018-09-05 12:39:50 -0300

    feat (exercício4): início do exercício 4 feito no Eclipse

---
+++ modulo01/src/Exercicio4/src/Elfo.java
@@ -0,0 +1,12 @@
+
+
+/*Exercício 04
+Na classe Elfo, adicione um atributo qtdFlechas (inteiro), que comece com o valor 7 e outro campo experiencia (inteiro), 
+que começa com 0. Também adicione um método atirarFlecha que diminui a quantidade atual de flechas do elfo a cada 
+chamada e aumente a experiência. 
+Seja cuidadoso com os casos de teste e evite situações que não fazem sentido.*/
+
+public class Elfo {
+	private String nome;
+	
+}
---
+++ modulo01/src/Exercicio4/src/teste/Teste.java
@@ -0,0 +1,10 @@
+package teste;
+import classe.Elfo;
+
+public class Teste {
+
+		Elfo orlando = new Elfo ("Orlando", 7, 0);
+		Elfo legolas = new Elfo ("Legolas", 7, 0);
+		
+		
+}
