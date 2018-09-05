5548afcec0731816f8dfb91b582473291eb54a8e
Jaqueline <jaqueline.bonoto@dbccompany.com.br>
2018-09-05 13:13:48 -0300

    fix (exercício 1 e 3): ajuste de testes.

--- modulo01/src/as_duas_torres/Numero.java
+++ modulo01/src/as_duas_torres/Numero.java
@@ -1,11 +1,33 @@
 public class Numero {
-    private int numero;
-    
-    public Numero (int numero1){
-        this.numero = numero1;    
-    }
-    
-    public boolean impar (){
-        return true;
-    }
+	private int numero;
+	
+	public Numero(int number) {
+		this.numero =number ;
+	}
+	
+	public boolean impar () {
+		if (numero %2 == 1) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public boolean verificarSomaDivisivel (int number) {
+		int soma=0;
+		while (numero>0) {
+			soma = soma +numero%10;
+			numero = numero/10;
+		}
+		if (soma!=0 || soma%numero==0){
+		    return true;
+		}else{
+			return false;
+		}
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+	
 }
---
+++ modulo01/src/as_duas_torres/NumeroTest.java
@@ -0,0 +1,31 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class NumeroTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class NumeroTest
+{
+    @Test    
+    public void testeImpar () {
+               
+        assertEquals(true, new Numero(5).impar());
+        assertEquals(true, new Numero(27).impar());
+        assertEquals(false, new Numero(2018).impar());
+        
+    }    
+    
+    @Test
+    public void testeSomaDigitos (){
+        assertEquals(true, new Numero(9).verificarSomaDivisivel(1892376));
+        assertEquals(true, new Numero(3).verificarSomaDivisivel(17));
+    }  
+        
+}
