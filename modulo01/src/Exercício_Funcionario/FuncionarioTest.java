e11cb6320f4455207a559edcadca5f6d8849117b
Jaqueline <jaqueline.bonoto@dbccompany.com.br>
2018-09-05 16:40:33 -0300

    refactor(exercicio funcionario): otimizacoes de aula

--- modulo01/src/Exercício_Funcionario/FuncionarioTest.java
+++ modulo01/src/Exercício_Funcionario/FuncionarioTest.java
@@ -9,8 +9,15 @@ public class FuncionarioTest
 {
     @Test
     public void testeLucro(){
-    	assertEquals(585.0, new Funcionario("Capitão América", 500.0, 1000.0).getLucro(), 0.0);
-    	assertEquals(1508.4675, new Funcionario("Rogue", 500.0, 7840.50).getLucro(), 0.0);
+        assertEquals(585.0, new Funcionario("Capitão América", 500.0, 1000.0).getLucro(), 0.0);
+        assertEquals(1508.4675, new Funcionario("Rogue", 500.0, 7840.50).getLucro(), 0.0);
     }
+    
+    @Test
+    public void joaoDeveLucrar1508(){
+        Funcionario joao = new Funcionario ("João", 500.0, 7840.50);
+        assertEquals(1508.4675, joao.getLucro(), 0.1);
+    }
+    
 
 }
