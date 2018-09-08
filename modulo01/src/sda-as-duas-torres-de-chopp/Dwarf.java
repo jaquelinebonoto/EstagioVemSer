322ae40bebeeb22271088202bd2b95c049086808
Bernardo Bosak de Rezende <bernardo.bosak@dbccompany.com.br>
2018-09-07 21:47:50 -0300

    feat(Dwarf): alterar status para morto quando vida ficar 0 e evitar que vida fique negativa.

--- modulo01/src/sda-as-duas-torres-de-chopp/Dwarf.java
+++ modulo01/src/sda-as-duas-torres-de-chopp/Dwarf.java
@@ -2,13 +2,17 @@ public class Dwarf {
     private String nome;
     private Status status = Status.VIVO;
     private double vida = 110.0;
+    private final double QTD_DANO = 10.0;
     
     public Dwarf(String nome) {
         this.nome = nome;
     }
     
     public void perderVida() {
-        vida -= 10.0;
+        vida -= this.vida >= QTD_DANO ? QTD_DANO : 0;
+        if (vida == 0.0) {
+            this.status = Status.MORTO;
+        }
     }
     
     public double getVida() {
