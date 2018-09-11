83bd32c98b3394d3c14b4d453181743a7d35cf3a
Jaqueline <jaqueline.bonoto@dbccompany.com.br>
2018-09-11 20:48:04 -0300

    retirada

--- modulo01/src/sda_as_duas_torres_jaque/Dwarf.java
+++
@@ -1,7 +0,0 @@
-public class Dwarf extends Personagem {
-
-    public Dwarf(String nome) {
-        super(nome, 110.0, Status.VIVO);
-        QTD_DANO = 10.0;
-    }
-}
--- modulo01/src/sda_as_duas_torres_jaque/ElfoNoturno.java
+++
@@ -1,7 +0,0 @@
-public class ElfoNoturno extends Elfo {
-    public ElfoNoturno(String nome) {
-        super(nome);
-        QTD_DANO = 15.;
-        QTD_EXPERIENCIA *= 3;
-    }
-}
