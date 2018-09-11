b6e311ceb957eb177ad325a39b6d909c80ecd044
Jaqueline <jaqueline.bonoto@dbccompany.com.br>
2018-09-10 21:12:01 -0300

    feat(Jogo): subindo novamente

---
+++ modulo01/src/sda_as_duas_torres_jaque/Elfo.java
@@ -0,0 +1,32 @@
+public class Elfo extends Personagem {
+    private int experiencia;
+    private Item arco;
+    private Item flecha;
+
+    // type initializer
+    {
+        experiencia = 0;
+        arco = new Item("Arco", 1);
+        flecha = new Item("Flecha", 7);
+    }
+
+    public Elfo(String nomeInformado) {
+        super(nomeInformado, 100.0, Status.VIVO);
+    }
+
+    public void atirarFlecha(Dwarf dwarf) {
+        if (flecha.getQuantidade() > 0) {
+            flecha.setQuantidade(flecha.getQuantidade() - 1);
+            experiencia++;
+            dwarf.perderVida();
+        }
+    }
+
+    public Item getFlecha() {
+        return this.flecha;
+    }
+
+    public int getExperiencia() {
+        return this.experiencia;
+    }
+}
