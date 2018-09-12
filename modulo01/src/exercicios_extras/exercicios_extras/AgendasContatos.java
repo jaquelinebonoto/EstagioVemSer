656f90abc41f5231133d3d6339af0bda551e640a
Jaqueline <jaqueline.bonoto@dbccompany.com.br>
2018-09-12 06:45:52 -0300

    Exercicio extra HashMap

---
+++ modulo01/src/exercicios_extras/exercicios_extras/AgendasContatos.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class AgendasContatos {
+    private HashMap<String, String> agendaContatos = new HashMap<>();
+
+    AgendaContatos agenda = new AgendaContatos();
+    agendaContatos.put("Bernardo", "555555");
+    agendaContatos.put("Mithrandir", "444444");
+    agendaContatos.consultar("Bernardo"); // “555555”
+
+    public void registrarContato(String nome, String telefone) {
+        ArrayList<agenda> listaAtual = this.registrarContato(nome);
+        boolean primeiroContato = listaAtual == null;
+        if (primeiroContato) {
+            listaAtual = new ArrayList<>();
+            dwarvesPorElfo.put(elfo, listaAtualDoElfo);
+        }
+        listaAtualDoElfo.add(telefone);
+    }
+
+    agenda.csv(); // retorna a seguinte string:
+}
+
