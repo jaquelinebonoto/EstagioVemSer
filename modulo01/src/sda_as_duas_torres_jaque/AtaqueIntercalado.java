2fe834b1b37eab433330edb5fbb67538c1f2737c
Jaqueline <jaqueline.bonoto@dbccompany.com.br>
2018-09-16 22:04:30 -0300

    feat(AtaqueIntercalado): teste com numero impar de elfos verde e noturno

---
+++ modulo01/src/sda_as_duas_torres_jaque/AtaqueIntercalado.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class AtaqueIntercalado implements Estrategias {
+    HashMap<Class, ArrayList<Elfo>> porClasse = new HashMap<>();
+
+    public List<Elfo> getOrdemDeAtaque(List<Elfo> atacantes) {
+        for (Elfo elfo : atacantes) { //recebe a lista de elfos
+            if (elfo.getStatus() == Status.VIVO) { //testa se está vivo
+                this.inicializarListaDeClasse(elfo.getClass());
+                porClasse.get(elfo.getClass()).add(elfo);
+            }
+        }
+        ArrayList<Elfo> ordenados = new ArrayList<>(); //o array onde os elfos serão colocados em ordem
+        ArrayList<Elfo> elfosVerdes = porClasse.get(ElfoVerde.class); //o array só com elfos verdes
+        ArrayList<Elfo> elfosNoturnos = porClasse.get(ElfoNoturno.class); //só com noturnos
+        int tamanhoIgualdade=0;
+        if (elfosVerdes.size()<elfosNoturnos.size()) tamanhoIgualdade=elfosVerdes.size();
+        else tamanhoIgualdade=elfosNoturnos.size();
+        int k=0;
+        for (int i = 0; i <= tamanhoIgualdade-1; i++) {
+            ordenados.add(k, elfosVerdes.get(i));
+            k++;
+            ordenados.add(k, elfosNoturnos.get(i));
+            k++;
+        }
+        return ordenados;
+    }
+
+    private void inicializarListaDeClasse(Class classe) { //
+        if (this.porClasse.get(classe) == null) {
+            ArrayList<Elfo> elfosPorClasse = new ArrayList<>();
+            porClasse.put(classe, elfosPorClasse);
+        }
+    }
+
+}
---
+++ modulo01/src/sda_as_duas_torres_jaque/AtaqueIntercaladoTest.java
@@ -0,0 +1,28 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.*;
+
+public class AtaqueIntercaladoTest {
+
+    @Test
+    public void elfosIntercaladosInformei5Elfos() {
+        Elfo legolas = new ElfoVerde("Legolas");
+        Elfo yasser = new ElfoNoturno("Mad Yasser");
+        Elfo ghilherme = new ElfoVerde("GG");
+        Elfo rachid = new ElfoNoturno("Yace");
+        Elfo henrique = new ElfoVerde("GhiGhi");
+
+        ArrayList<Elfo> elfos = new ArrayList(
+                Arrays.asList(legolas, yasser, ghilherme, rachid, henrique)
+            );
+        AtaqueIntercalado formacao = new AtaqueIntercalado();
+        ArrayList<Elfo> esperado = new ArrayList(
+                Arrays.asList(legolas, yasser, ghilherme, rachid)
+            );
+        assertEquals(esperado, formacao.getOrdemDeAtaque(elfos));
+    }
+}
