2ee67402c9e64dd87267bd6ef4d8aef8e385a7af
Jaqueline <jaqueline.bonoto@dbccompany.com.br>
2018-09-17 14:00:47 -0300

    ajeitando o desktop na dbc

---
+++ modulo01/src/sda_as_duas_torres_jaque/DadoD6Test.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DadoD6Test{
+    
+    @Test
+    public void DadoD6Test(){
+        DadoD6 dado = new DadoD6 ();
+        assertEquals (4, dado.sortear());
+    }
+
+    
+}
