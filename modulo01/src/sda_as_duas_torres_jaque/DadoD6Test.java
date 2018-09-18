f39def4721384b3e6ffbf780e17e2684a19ffbeb
Jaqueline <jaqueline.bonoto@dbccompany.com.br>
2018-09-18 13:38:29 -0300

    update

--- modulo01/src/sda_as_duas_torres_jaque/DadoD6Test.java
+++ modulo01/src/sda_as_duas_torres_jaque/DadoD6Test.java
@@ -5,11 +5,12 @@ import org.junit.Test;
 
 public class DadoD6Test{
     
+    /*
     @Test
     public void DadoD6Test(){
         DadoD6 dado = new DadoD6 ();
         assertEquals (4, dado.sortear());
     }
 
-    
+    */
 }
