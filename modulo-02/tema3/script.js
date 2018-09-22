fb50b998cd9965d08e0b6864a97a717407ec1c2e
Jaqueline <jaqueline.bonoto@dbccompany.com.br>
2018-09-22 18:44:01 -0300

    feat(Exercicio 2): para verificar se o ano é bissexto

--- modulo-02/tema3/script.js
+++ modulo-02/tema3/script.js
@@ -37,3 +37,34 @@ console.log(objeto.calcularCirculo(objeto));
 console.log(objeto2.calcularCirculo(objeto2));
 console.log(objeto3.calcularCirculo(objeto3));
 console.log(objeto4.calcularCirculo(objeto4));
+
+console.log("------------------------------------------------------------------");
+/*Crie uma função naoBissexto que recebe um ano (número) e verifica se ele não é bissexto. Exemplo:
+	bissexto(2016) // false
+    bissexto(2017) // true */
+    
+    var ano2016 = {
+        dias: 366,
+        naoBissexto
+    }
+
+    var ano2018 = {
+        dias: 365,
+        naoBissexto
+    }
+    function naoBissexto (ano){
+        if (this.dias === 366) return true;
+        return false;
+    }
+    console.log("Ano 2016 é bissexto?   " + ano2016.naoBissexto(ano2016));
+    console.log("Ano 2018 é bissexto?   " + ano2018.naoBissexto(ano2018));
+    
+    console.log("------------------------------------------------------------------");
+    
+    /*Crie uma função concatenarSemUndefined que recebe duas variáveis. Caso qualquer uma delas seja undefined (cuide a melhor forma de verificar isso, foi visto em aula), considere “” no lugar. Exemplo:
+
+	concatenarSemUndefined( undefined, “Soja” ) // “Soja”
+	concatenarSemUndefined( “Soja”, “ é bom” ) // “Soja é bom”
+    concatenarSemUndefined( “Soja é” ) // “Soja é”*/
+    
+    
