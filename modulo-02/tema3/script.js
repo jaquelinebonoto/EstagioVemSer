7ec1495fc588d54f9264dce3ab8400df0f0e8bf9
Jaqueline <jaqueline.bonoto@dbccompany.com.br>
2018-09-23 15:25:16 -0300

    feat(exercicios 5 e 6): inicio do desevolvimento.

--- modulo-02/tema3/script.js
+++ modulo-02/tema3/script.js
@@ -110,11 +110,6 @@ console.log("------------------------------------------------------------------"
 	concatenarSemNull( “Soja”, “ é bom” ) // “Soja é bom”
     concatenarSemNull( “Soja é” ) // “Soja éundefined”*/
     
-    function queroQueDeErro (data){
-        console.log(data);
-        return(data)
-    }
-
     var coiso = {
         name: "coisa",
         concatenarSemNull
@@ -135,8 +130,98 @@ console.log("------------------------------------------------------------------"
             console.log(nome+""); 
         } else console.log(nome + outroNome);
     }
+
+    function concatenarSemUndefined (nome, outroNome){
+        if (nome === undefined){
+            console.log(""+outroNome);
+        } else if (outroNome === undefined){
+            console.log(nome+""); 
+        } else console.log(nome + outroNome);
+    }
+
+    function testePraDarErrado (circulo) {
+        if (this.tipoCalculo==="A"){
+            return (Math.PI * Math.pow(this.raio, 2))
+        }
+    }
     var foo = null;
     coiso.concatenarSemNull(coiso.name, coisaSeria.name);
     coiso.concatenarSemNull(coiso.name, doida.name);
     coiso.concatenarSemNull(coiso.name, foo);
     coiso.concatenarSemNull(foo, doida.name);
+    coiso.concatenarSemNull(testePraDarErrado(doida), doida.name);
+    
+
+    console.log("------------------------------------------------------------------");
+
+    /*Crie uma função concatenarEmPaz que recebe duas variáveis. 
+    Caso qualquer uma delas seja null ou undefined (cuide a melhor forma de verificar isso,
+    foi visto em aula), considere “” no lugar. Exemplo
+
+	concatenarEmPaz( undefined, “Soja” ) // “Soja”
+	concatenarEmPaz( “Soja é bom, mas...”, null ) // “Soja é bom, mas...”
+    concatenarEmPaz( “Soja é” ) // “Soja é”*/
+    
+    var coiso = {
+        name: "coisa",
+        concatenarEmPaz
+    }
+    var coisaSeria = {
+        name: " cousaSeria",
+        concatenarEmPaz
+    }  
+    var doida = {
+        name: " doida",
+        concatenarEmPaz
+    }
+    function concatenarEmPaz (nome, outroNome){
+        if (nome === null || nome === undefined){
+            console.log(""+outroNome);
+        } else if (outroNome === null || outroNome === undefined){
+            console.log(nome+""); 
+        } else console.log(nome + outroNome);
+    }
+
+    coiso.concatenarEmPaz(coiso.name, coisaSeria.name);
+    coiso.concatenarEmPaz(coiso.name, doida.name);
+    coiso.concatenarEmPaz(coiso.name, foo);
+    coiso.concatenarEmPaz(foo, doida.name);
+    coiso.concatenarEmPaz(testePraDarErrado(doida), doida.name);
+
+    console.log("------------------------------------------------------------------");
+    /*Escreva uma função adicionar que permite somar dois números através de duas 
+    chamadas diferentes (não necessariamente pra mesma função). Pirou? Ex:
+    adicionar(3)(4) // 7       adicionar(5642)(8749) // 14391*/
+
+    function adicionar (a, b) {
+        return a + b;
+      }
+      
+    console.log(adicionar(3, 4));
+    console.log(adicionar(5642, 8749));
+    console.log(adicionar(-12, 4));
+    console.log(adicionar(0, -1));
+
+    console.log("------------------------------------------------------------------");
+    /*Faça uma função fiboSum que calcule a soma da sequência de Fibonacci para n números informados. Exemplo de chamada:
+    fiboSum(7)  // 33 (soma dos 7 primeiros números da sequência: 1+1+2+3+5+8+13)
+    Dica: aproveite toda "beleza" dos algoritmos recursivos (com cuidado). */
+
+    function fiboSum (numero){ //somando os valores até chegar no valor 7
+        if (numero==0) return 0;
+        if (numero==1) return 1;
+        return fiboSum(numero-1) + fiboSum (numero-2);
+    }
+    
+    console.log(fiboSum(7));
+    console.log(fiboSum(33));
+
+    var soma=1;
+    var outro=0;
+    function fiboSoma (numero){ //somando os valores até ter 7 valores
+        for (i=1; i<=numero; i++){
+            soma = soma + (numero-1) + numero-2;
+        }
+        return soma;
+    }
+    console.log(fiboSoma(7));
