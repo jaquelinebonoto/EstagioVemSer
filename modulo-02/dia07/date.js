c52ba106030ac4b89110a746f02836eeaab2132e
Jaqueline <jaqueline.bonoto@dbccompany.com.br>
2018-09-26 18:55:21 -0300

    feat(Ex lista 5 e dia 07): arrumando a bagunça  mas credo

---
+++ modulo-02/dia07/date.js
@@ -0,0 +1,27 @@
+function rodarPrograma(){
+    const h1 = document.getElementById('horario')
+    
+    const relogio = new Date().toLocaleTimeString() //declarando a dataa
+    h1.innerText = relogio //chamando a data antes de cahmar a função que atualiza
+    setInterval(function(){ //atualizando a data no html
+     h1.innerText = relogio
+    }, 1000)
+    
+    
+    //isso mostra uma característica assíncrona. porque a função é executada e continua enfileirando
+    setInterval(function(){ //nesta estamos dando saida no console
+      console.log(`oi ${new Date()}`), 016415561
+    })
+  
+    const idIntervalo = setInterval (atualizarH1, 1000)
+    const btnPararRelogio = document.getElementById('btnPararRelogio')
+    btnPararRelogio.onclick = function(){
+        clearInterval( idIntervalo)
+    }
+    //window.setInterval()
+  
+    //setTimeout //posterga a execução e executa só uma vez
+  
+    const dataAgora = new Date
+  }
+  rodarPrograma()
