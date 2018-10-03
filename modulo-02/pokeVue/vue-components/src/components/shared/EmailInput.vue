af88ce2eb5a9d252698d2643d1c31dddccd79d21
Jaqueline <jaqueline.bonoto@dbccompany.com.br>
2018-10-03 17:31:40 -0300

    feat(VeeValidate): teste com vee validate

--- modulo-02/pokeVue/vue-components/src/components/shared/EmailInput.vue
+++ modulo-02/pokeVue/vue-components/src/components/shared/EmailInput.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="form-group">
     <label for="exampleInputEmail1"></label>
-    <input type="email" v-bind:name="nome" v-model="valor" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="usuario@email.com">
+    <input type="text" v-validate=" 'required|email' " v-bind:name="nome" v-model="valor" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="usuario@email.com">
+    <span>{{errors.first( this.nome )}}</span>
   </div>
 </template>
 <script>
--- modulo-02/pokeVue/vue-components/src/main.js
+++ modulo-02/pokeVue/vue-components/src/main.js
@@ -1,11 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VeeValidate , { Validator } from 'vee-validate'
 import App from './App.vue'
 import Login from './components/screens/Login.vue'
 import Home from './components/screens/Home.vue'
+import ptBR from 'vee-validate/dist/locale/pt_BR'
+
+
+Validator.localize('pt_BR', ptBR);
 
 Vue.config.productionTip = false
 Vue.use( VueRouter )
+Vue.use(VeeValidate)
+
+
+
 
 const routes = [
   { path: '/', component: Login },
